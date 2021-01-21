506b0caeb4b96d9cee3522150c8ecc4c3bb28fa9
chrissciwilliams <christopher.sci.williams@gmail.com>
2021-01-21 15:51:21 -0500

    misc bugfixes

--- ssbondca.py
+++ ssbondca.py
@@ -18,7 +18,7 @@ def filesetter(pdbfile):
   return currentfile
 
 def parseatom(fileline): 
-    res_id = fileline[22].strip() + ':' + fileline[23:26].strip() + ':' + fileline[27].strip() 
+    res_id = fileline[22].strip() + ':' + fileline[23:26] + ':' + fileline[27].strip() 
     c_alpharesidlist.append(res_id)
     c_alphas[res_id] = {"X": float(fileline[31:38].strip()), "Y": float(fileline[39:46].strip()), "Z": float(fileline[47:54].strip())}
     return res_id
@@ -34,8 +34,8 @@ def filesetter(pdbfile):
   return currentfile
 
 def ssbondreslists(fileline): 
-    ssbondres1list.append(fileline[16].strip() + ':'+ abs(int(fileline[18:21].strip())) + ':' fileline[22].strip())
-    ssbondres2list.append([16].strip() + ':'+ abs(int(fileline[32:35].strip())) + ':' fileline[22].strip())
+    ssbondres1list.append(fileline[16].strip() + ':'+ fileline[18:21] + ':' + fileline[22].strip())
+    ssbondres2list.append(fileline[16].strip() + ':'+ fileline[32:35] + ':' + fileline[22].strip())
 
 
 for line in pdbsetoffiles: 
@@ -44,26 +44,23 @@ for line in pdbsetoffiles:
    for fileline in currentfile:
        if fileline.startswith("TITLE     "): 
             print("The name of the structure that the following information will be under is this: " + fileline.strip())
-       if str(fileline[0:5]) == "SSBOND":
+       if str(fileline[0:6]) == "SSBOND": #fileline.startswith("SSBOND")
+       #12345678901234567890
+       #SSBOND *** CYS A   23    CYS A   88                          1555   1555  2.04
           
           ssbondreslists(fileline)
-          print(str(fileline[7:9].strip()) + "," + str(fileline[11:13].strip()) + "," + str(fileline[15].strip()) + "," + str(fileline[17:20].strip()) + str(fileline[21].strip()) + "," + str(fileline[25:27].strip()) + "," + str(fileline[29].strip()) + "," + str(fileline[31:34].strip()) + "," + str(fileline[35].strip()) + '\n')
-    for fileline in currentfile: 
+          print(str(fileline[7:10].strip()) + "," + str(fileline[11:14].strip()) + "," + str(fileline[15].strip()) + "," + str(fileline[17:20].strip()) + str(fileline[21].strip()) + "," + str(fileline[25:27].strip()) + "," + str(fileline[29].strip()) + "," + str(fileline[31:34].strip()) + "," + str(fileline[35].strip()) + '\n')
+   for fileline in currentfile:
 
         #this is where I am calculating the distances     
-        #if fileline.startswith("ATOM"): 
-            #res_id = parseatom(fileline)
-                  #if fileline[13:16] == " CA ": 
-                      #for x in ssbondres1list:
-                          #for y in c_alpharesidlist: 
-                              #if x == y: 
-                                   #for z in ssbondres2list: 
-                                       #if y == z: 
-
-                                       # distance = abs(((c_alphas.y("X")) - (c_alphas.z("X")))
-                         #d = ((X1-X2)^2+(Y1-Y2)**2+(Z1-Z2)**2)**0.5
-                         
-        
-
-
+        if fileline.startswith("ATOM"): 
+            res_id = parseatom(fileline)
+            if fileline[13:16] == " CA ": 
+                      for x in ssbondres1list:
+                          for y in c_alpharesidlist: 
+                              if x == y: 
+                                   for z in ssbondres2list: 
+                                       if y == z: 
+                                            distance = abs(((c_alphas.y("X")) - (c_alphas.z("X"))))
+                         #d = ((X1-X2)**2+(Y1-Y2)**2+(Z1-Z2)**2)**0.5
 
