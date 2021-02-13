fd8746f0ad0d4379041456725950fe1eb2d0fab9
spasumarthy <sushritp1@gmail.com>
2021-02-13 11:06:51 -0500

    Update ssbondca.py

--- ssbondca.py
+++ ssbondca.py
@@ -26,12 +26,15 @@ def filesetter(pdbfile):
 
 
 def gothroughCArecords(currentfile): 
+
   for fileline in currentfile: 
+    
+    
     if fileline.startswith("ATOM") and fileline[12:16] == " CA ": 
       #explicit parsing of the line helps with readability and debugging
-      chainid = fileline[22]
-      resseq = fileline[23:26]
-      inscode = fileline[27]
+      chainid = fileline[21]
+      resseq = fileline[22:26]
+      inscode = fileline[26]
       ### Debug parsing ###
       #print("|"+chainid+"|"+resseq+"|"+inscode+"|")
       #sys.exit()
@@ -42,31 +45,41 @@ def gothroughCArecords(currentfile):
         if cares_id == element: 
           for fileline1 in currentfile: 
             if fileline1.startswith("ATOM") and fileline1[12:16] == " CA ":
-              Xcares_id = fileline1[22].strip() + fileline1[23:26].strip() + fileline1[27].strip()
+              Xcares_id = fileline1[21].strip() + fileline1[22:26].strip() + fileline1[26].strip()
               if Xcares_id == ssbondres2list.index(element): 
                 distance = ((fileline1[31:38]-fileline[31:38])**2+(fileline1[31:38]-fileline[31:38])**2 + (fileline1[31:38]-fileline[31:38])**2)**0.5
                 print(distance)
-        elif cares_id in ssbondres2list: 
-          for fileline2 in currentfile: 
-            if fileline2.startswith("ATOM") and fileline2[12:16] == " CA ":
-              Ycares_id = fileline1[22].strip() + fileline1[23:26].strip() + fileline1[27].strip()
-              if Ycares_id == ssbondres2list(ssbondres2list.index(cares_id)): 
-                distance = ((fileline1[31:38]-fileline[31:38])**2+(fileline1[31:38]-fileline[31:38])**2 + (fileline1[31:38]-fileline[31:38])**2)**0.5
+                
+                #add else statement
+      #  elif cares_id in ssbondres2list: 
+       #   for fileline2 in currentfile: 
+        #    if fileline2.startswith("ATOM") and fileline2[12:16] == " CA ":
+         #     Ycares_id = fileline1[21].strip() + fileline1[22:26].strip() + fileline1[26].strip()
+          #    if Ycares_id == ssbondres2list(ssbondres2list.index(cares_id)): 
+           #     distance = ((fileline1[31:38]-fileline[31:38])**2+(fileline1[31:38]-fileline[31:38])**2 + (fileline1[31:38]-fileline[31:38])**2)**0.5
 
 for line in pdbsetoffiles: 
-  print(line.strip(),file=sys.stderr)
+  #print(line.strip(),file=sys.stderr)
   currentfile = filesetter(line)
   for fileline in currentfile: 
     if str(fileline[0:6]) == "SSBOND": #fileline.startswith("SSBOND")
        #12345678901234567890
        #SSBOND *** CYS A   23    CYS A   88                          1555   1555  2.04
-          
+      chainid1 = fileline[15]
+      resseq1 = fileline[17:21]
+      inscode1 = fileline[21]    
           #res_id is chain, num, and icode
-      res_id1 =  fileline[16].strip() + "," + fileline[17:21].strip() + "," + fileline[22].strip()
-      res_id2 = fileline[30].strip() + "," + fileline[31:35].strip() + "," + fileline[36].strip()
+      chainid2 = fileline[29]
+      resseq2 = fileline[31:35]
+      inscode2 = fileline[35]
+
+      res_id1 =  chainid1 + resseq1 + inscode1
+      res_id2 = chainid2 + resseq2 + inscode2 
       ssbondres1list.append(res_id1)
+
       ssbondres2list.append(res_id2)
          # print(str(fileline[7:10].strip()) + "," + str(fileline[11:14].strip()) + "," + str(fileline[15].strip()) + "," + str(fileline[17:21].strip()) + str(fileline[22].strip()) + "," + str(fileline[25:28].strip()) + "," + str(fileline[29].strip()) + "," + str(fileline[31:35].strip()) + "," + str(fileline[36].strip()))
-
-  currentfile = filesetter(line)
+    
+  #currentfile = filesetter(line)
+  currentfile.seek(0)
   gothroughCArecords(currentfile)
