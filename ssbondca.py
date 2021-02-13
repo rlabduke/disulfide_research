dcf8b7f581748c3e3e8617c4d2e98e2d97d9ff1f
chrissciwilliams <christopher.sci.williams@gmail.com>
2021-02-13 11:20:13 -0500

    file looping management

--- ssbondca.py
+++ ssbondca.py
@@ -25,11 +25,9 @@ def filesetter(pdbfile):
 
 
 
-def gothroughCArecords(currentfile): 
-
+def gothroughCArecords(pdbfile):
+  currentfile = filesetter(pdbfile)
   for fileline in currentfile: 
-    
-    
     if fileline.startswith("ATOM") and fileline[12:16] == " CA ": 
       #explicit parsing of the line helps with readability and debugging
       chainid = fileline[21]
@@ -42,11 +40,22 @@ def gothroughCArecords(currentfile):
       cares_id = chainid + resseq + inscode
       #cares_id = fileline[22].strip() + fileline[23:26].strip() + fileline[27].strip()
       for element in ssbondres1list: 
-        if cares_id == element: 
-          for fileline1 in currentfile: 
+        if cares_id == element:
+          loop2file = filesetter(pdbfile)
+          for fileline1 in loop2file: 
             if fileline1.startswith("ATOM") and fileline1[12:16] == " CA ":
-              Xcares_id = fileline1[21].strip() + fileline1[22:26].strip() + fileline1[26].strip()
-              if Xcares_id == ssbondres2list.index(element): 
+              chainid = fileline[21]
+              resseq = fileline[22:26]
+              inscode = fileline[26]
+              #Xcares_id = fileline1[21].strip() + fileline1[22:26].strip() + fileline1[26].strip()
+              Xcares_id = chainid + resseq + inscode
+              if Xcares_id == ssbondres2list.index(element):
+                x1 = 
+                y1 = 
+                z1 = 
+                x2 = 
+                y2 = 
+                z2 = 
                 distance = ((fileline1[31:38]-fileline[31:38])**2+(fileline1[31:38]-fileline[31:38])**2 + (fileline1[31:38]-fileline[31:38])**2)**0.5
                 print(distance)
                 
@@ -58,9 +67,9 @@ def gothroughCArecords(currentfile):
           #    if Ycares_id == ssbondres2list(ssbondres2list.index(cares_id)): 
            #     distance = ((fileline1[31:38]-fileline[31:38])**2+(fileline1[31:38]-fileline[31:38])**2 + (fileline1[31:38]-fileline[31:38])**2)**0.5
 
-for line in pdbsetoffiles: 
+for pdbfile in pdbsetoffiles: 
   #print(line.strip(),file=sys.stderr)
-  currentfile = filesetter(line)
+  currentfile = filesetter(pdbfile)
   for fileline in currentfile: 
     if str(fileline[0:6]) == "SSBOND": #fileline.startswith("SSBOND")
        #12345678901234567890
@@ -80,6 +89,6 @@ for line in pdbsetoffiles:
       ssbondres2list.append(res_id2)
          # print(str(fileline[7:10].strip()) + "," + str(fileline[11:14].strip()) + "," + str(fileline[15].strip()) + "," + str(fileline[17:21].strip()) + str(fileline[22].strip()) + "," + str(fileline[25:28].strip()) + "," + str(fileline[29].strip()) + "," + str(fileline[31:35].strip()) + "," + str(fileline[36].strip()))
     
-  #currentfile = filesetter(line)
-  currentfile.seek(0)
-  gothroughCArecords(currentfile)
+  #currentfile = filesetter(pdbfile)
+  #currentfile.seek(0)
+  gothroughCArecords(pdbfile)
